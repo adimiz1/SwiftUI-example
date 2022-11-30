996e93811f61c8206faf4ef35c6b831297d5004f
mizadi <adi.mizrahi@cloudinary.com>
2022-11-30 08:26:28 +0200

    Initial Commit

---
+++ Cloudinary-SwiftUI-Sample/Cloudinary_SwiftUI_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cloudinary_SwiftUI_SampleApp.swift
+//  Cloudinary-SwiftUI-Sample
+//
+//  Created by Adi Mizrahi on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Cloudinary_SwiftUI_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
