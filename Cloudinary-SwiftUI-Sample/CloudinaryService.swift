9f43e5cccf9406ae9a5f7acae95bbe0968e6384e
mizadi <adi.mizrahi@cloudinary.com>
2022-12-05 22:49:02 +0200

    Add .gitignore

---
+++ Cloudinary-SwiftUI-Sample/CloudinaryService.swift
@@ -0,0 +1,8 @@
+//
+//  CloudinaryService.swift
+//  Cloudinary-SwiftUI-Sample
+//
+//  Created by Adi Mizrahi on 05/12/2022.
+//
+
+import Foundation
---
+++ Cloudinary-SwiftUI-Sample/Uploader.swift
@@ -0,0 +1,8 @@
+//
+//  Uploader.swift
+//  Cloudinary-SwiftUI-Sample
+//
+//  Created by Adi Mizrahi on 30/11/2022.
+//
+
+import Foundation
